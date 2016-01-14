8d3a5e5a2c7194b3a3410ded10bdb4f39b62099a
Emil Fresk <emil.fresk@gmail.com>
2016-01-14 15:52:29 +0100

    Added commenting.

--- include/viconstream/viconstream.h
+++ include/viconstream/viconstream.h
@@ -55,27 +55,78 @@ private:
     /** @brief Vector holding the registered callbacks. */
     std::map<unsigned int, viconstream_callback> callbacks;
 
+    /** @brief Vicon client object. */
     Client _vicon_client;
+
+    /** @brief The vicon server's address. */
     std::string _host_name;
+
+    /** @brief Time since the ViconStream object was created. */
     std::chrono::high_resolution_clock::time_point _tp_start;
+
+    /** @brief Reference to the output stream for logging. */
     std::ostream &_log;
+
+    /** @brief Mutex for accessing the log. */
     std::mutex _log_lock;
 
+    /** @brief Thread object for the frame grabber. */
     std::thread _frame_grabber;
+
+    /** @brief Shutdown selector for the frame grabber and callback worksers. */
     volatile bool _shutdown;
 
+    /**
+     * @brief   Logs a string to the log output stream.
+     *
+     * @param[in] log   The string to be logged.
+     */
     void logString(const std::string log);
+
+    /**
+     * @brief   The frame grabber's worker function.
+     */
     void frameGrabberWorker();
+
+    /**
+     * @brief   The callback sender's worker function.
+     */
     void callbackWorker();
 
 public:
+
+    /**
+     * @brief   Constructor for the ViconStream.
+     *
+     * @param[in] hostname   Address to the Vicon server.
+     * @param[in] log_output Reference to the log output stream.
+     */
     ViconStream(std::string hostname, std::ostream &log_output);
+
+    /**
+     * @brief   Destructor handles the graceful exit of the ViconStream.
+     */
     ~ViconStream();
+
+    /**
+     * @brief   Enables the Vicon stream and starts receiving data.
+     *
+     * @param[in] enableSegmentData         Request segment data.
+     * @param[in] enableMarkerData          Request marker data.
+     * @param[in] enableUnlabeledMarkerData Request unlabeled marker data.
+     * @param[in] enableDeviceData          Request device data.
+     *
+     * @return  Returns true if the stream was started correctly.
+     */
     bool enableStream(bool enableSegmentData = true,
                       bool enableMarkerData = false,
                       bool enableUnlabeledMarkerData = false,
                       bool enableDeviceData = false,
                       StreamMode::Enum streamMode = StreamMode::ServerPush);
+
+    /**
+     * @brief   Disabled the vicon stream.
+     */
     void disableStream();
 
     /**
